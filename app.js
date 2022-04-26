0b5c8c222597e54dae8b88a3787fd2090c4ce8d6
Ysis Numeriano <ysisnumeriano2@gmail.com>
2022-04-26 16:28:36 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
+    fetch(getPokemonUrl(index + 1)).then(response => response.json()))
+const generateHTML = pokemons => {
+               
+        return pokemons.reduce((accumulator, pokemon) => {
+           const types = pokemon.types.map(typeInfo => typeInfo.type.name) 
+            accumulator += `
+            <li class="card" ${types[0]}>
+            <img glass="card-image " alt="${name}"
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
+                <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                <p class="card-subtitle">${types.join(' | ')}</p>
+            </li>
+            `
+            return accumulator
+        }, '')                       
+    
+
+}
+const insertPokemonIntoPage = pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"]')
+
+    ul.innerHTML = pokemons
+
+}
+
+
+const pokemonPromises = generatePokemonPromises()
+
+Promise.all(pokemonPromises)
+    .then(generateHTML)
+
+    .then(insertPokemonIntoPage)
+
+
+        
+    
+   
